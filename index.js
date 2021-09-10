fc32e0fa87f87ee700abbb3ce38e98c7e07c1a9e
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-09-10 21:56:24 +0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>:)')
+  res.send('<h1>Hello World!</h1>^.^')
 })
 
 app.listen(port, () => {
