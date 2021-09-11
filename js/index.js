779568e5196afc29277a0a15700585038df921f6
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-09-11 15:49:34 +0300

    added modal for new employee

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-$("#btn").click(function () {
-  alert("aesgsh");
-});
+window.onload = () =>{
+
+  // document.getElementById("add-employee-button").addEventListener("click", AddEmployee, false);
+
+  document.getElementById("modalButton").addEventListener("click", openModal, true);
+
+}
+
+function openModal() {
+  document.getElementById('myModal').style = "display:block";
+  document.getElementById('myModal').classList.add("show");
+}
